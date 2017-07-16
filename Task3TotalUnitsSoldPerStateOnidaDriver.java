447abb51907c703bfee1fb31671784b17dbf577e
shyamalaramesh <shyamala_gg@yahoo.co.uk>
2017-07-16 23:21:59 +0530

    Add files via upload

---
+++ Task3TotalUnitsSoldPerStateOnidaDriver.java
@@ -0,0 +1,76 @@
+package mapreduce.task3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Task3TotalUnitsSoldPerStateOnidaDriver {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+		// initialize the configuration
+		Configuration conf = new Configuration();
+		
+		// create a job object from the configuration and give it any name you want 
+		Job job = new Job(conf, "Assignment_4.2 -> Task_3 -> "
+				+ "Number_Of_Onida_Units_Sold_Per_State");
+		
+		// java.lang.Class object of the driver class
+		job.setJarByClass(Task3TotalUnitsSoldPerStateOnidaDriver.class);
+
+		// map function outputs key-value pairs. 
+		// What is the type of the key in the output 
+		job.setMapOutputKeyClass(Text.class);
+		// map function outputs key-value pairs. 
+		// What is the type of the value in the output
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		// reduce function outputs key-value pairs. 
+		// What is the type of the key in the output. 
+		// In this case output is number of Onida units sold per state. 
+		// So key type is Text 
+		job.setOutputKeyClass(Text.class);
+		// reduce function outputs key-value pairs. 
+		// What is the type of the value in the output. 
+		// In this case output is number of Onida units sold per state. 
+		// So value type is IntWritable 
+		job.setOutputValueClass(IntWritable.class);
+		
+		// Mapper class which has implemenation for the map phase
+		job.setMapperClass(Task3TotalUnitsSoldPerStateOnidaMapper.class);
+		
+		// Reducer class which has implemenation for the map phase
+		job.setReducerClass(Task3TotalUnitsSoldPerStateOnidaReducer.class);
+		
+		// Input is a text file. So input format is TextInputFormat
+		job.setInputFormatClass(TextInputFormat.class);
+		
+		// Output is also a text file. So output format is TextOutputFormat
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		/*
+		 * The input path to the job. The map task will
+		 * read the files in this path form HDFS 
+		 */
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		
+		/*
+		 * The output path from the job. The map/reduce task will
+		 * write the files to this path to HDFS. In this case the 
+		 * reduce task will write to output path because number of 
+		 * reducer tasks is not explicitly configured to be zero  
+		 */
+		FileOutputFormat.setOutputPath(job,new Path(args[1]));
+		
+		job.waitForCompletion(true);
+	}
+}
+
+    Contact GitHub API Training Shop Blog About 
+
+
---
+++ Task3TotalUnitsSoldPerStateOnidaReducer.java
@@ -0,0 +1,30 @@
+package mapreduce.task3;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class Task3TotalUnitsSoldPerStateOnidaReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+	
+	/**
+	 * The mapper outputs stateName as key and value as 1
+	 * Now the reducer will take a particular stateName 
+	 * and the list of values associated with it. It will sum up these values and
+	 * output stateName as key and the total as the value thereby giving the total 
+	 * units sold in each state for Onida
+	 */
+	@Override
+	protected void reduce(Text stateName, Iterable<IntWritable> listOfUnitsSold,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context context) 
+					throws IOException, InterruptedException {
+		int totalOnidaUnitsSoldInAState = 0;
+		for(IntWritable unitsSold : listOfUnitsSold) {
+			totalOnidaUnitsSoldInAState += unitsSold.get();
+		}
+		context.write(stateName, new IntWritable(totalOnidaUnitsSoldInAState));
+	}
+	
+}
